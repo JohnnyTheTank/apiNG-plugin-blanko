41b32c8399a49e633842eaa0125f8cd2693dd7b4
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-01-11 15:36:36 +0100

    updated to new apiNG structure

--- demo/aping-config.js
+++ demo/aping-config.js
@@ -1,15 +1,8 @@
 "use strict";
 apingApp.config(['$provide', function ($provide) {
 
-    $provide.constant("apingApiKeys", {});
-
-    $provide.constant("apingDefaultSettings", {
-        templateUrl : "aping_design_sample.html",
-        items : 20, //items per request
-        maxItems: 100, //max items per aping
-        orderBy : "timestamp",
-        orderReverse : "true",
-        model: "native",
+    $provide.value("apingDefaultSettings", {
+        apingApiKeys: {}
     });
 
 }]);
